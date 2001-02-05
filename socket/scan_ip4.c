dc60cb52bd78b5287b55ad9b4b48d1c9d69b8b2a
leitner <leitner>
2001-02-05 22:09:26 +0000

    added ip[46] scanning routines.

---
+++ socket/scan_ip4.c
@@ -0,0 +1,19 @@
+#include "scan.h"
+#include "ip4.h"
+
+unsigned int scan_ip4(const char *s,char ip[4])
+{
+  unsigned int len;
+  unsigned long u;
+  int i;
+
+  len = 0;
+  for (i=0; i<4; ++i) {
+    register unsigned int j;
+    len+=(j=scan_ulong(s,&u))+1;
+    if (!j) return 0;
+    ip[i]=u; s+=j;
+    if (*s!='.') return 0; ++s;
+  }
+  return len-1;
+}
