ffd615858975ddb408fdc0336cfb55e597332162
sanjiyan <sanjiyan>
2001-08-23 19:01:52 +0000

    removed div with a shift

--- fmt/fmt_8long.c
+++ fmt/fmt_8long.c
@@ -3,7 +3,7 @@
 unsigned int fmt_8long(char *dest,unsigned long i) {
   register unsigned long len,tmp;
   /* first count the number of bytes needed */
-  for (len=1, tmp=i; tmp>7; ++len) tmp/=8;
+  for (len=1, tmp=i; tmp>7; ++len) tmp>>=3;
   if (dest)
     for (tmp=i, dest+=len; ; ) {
       *--dest = (tmp&7)+'0';
