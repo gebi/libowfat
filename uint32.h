980e24e92ac93c9fa4ef8090f09069ae6d071794
leitner <leitner>
2002-04-15 19:47:09 +0000

    oooops, forgot brackets

--- uint32.h
+++ uint32.h
@@ -6,7 +6,7 @@ typedef unsigned int uint32;
 #if defined(__i386__) && !defined(NO_UINT32_MACROS)
 #define uint32_pack(out,in) (*(uint32*)(out)=(in))
 #define uint32_unpack(in,out) (*(out)=*(uint32*)(in))
-#define uint32_read(in) (*(uint32*)in)
+#define uint32_read(in) (*(uint32*)(in))
 extern void uint32_pack_big(char *out,uint32 in);
 extern void uint32_unpack_big(const char *in,uint32* out);
 extern uint32 uint32_read_big(const char *in);
