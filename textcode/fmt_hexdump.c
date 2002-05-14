58334b0ea2bcc56bbc17475ad67df1561e80ad67
leitner <leitner>
2002-05-14 20:02:33 +0000

    new hexdump fmt and scan routines for textcode

---
+++ textcode/fmt_hexdump.c
@@ -0,0 +1,19 @@
+#include "fmt.h"
+#include "textcode.h"
+#include "str.h"
+#include "haveinline.h"
+
+static inline int tohex(char c) {
+  return c>9?c-10+'A':c+'0';
+}
+
+unsigned int fmt_hexdump(char* dest,const char* src,unsigned int len) {
+  register const unsigned char* s=(const unsigned char*) src;
+  unsigned long written=0,i;
+  for (i=0; i<len; ++i) {
+    dest[written]=tohex(s[i]>>4);
+    dest[written+1]=tohex(s[i]&15);
+    written+=2;
+  }
+  return written;
+}
---
+++ textcode/scan_hexdump.c
@@ -0,0 +1,27 @@
+#include "fmt.h"
+#include "textcode.h"
+#include "haveinline.h"
+
+static inline int fromhex(char c) {
+  if (c>='0' && c<='9') return c-'0';
+  if (c>='A' && c<='F') return c-'A'+10;
+  if (c>='a' && c<='f') return c-'a'+10;
+  return -1;
+}
+
+unsigned int scan_hexdump(const char *src,char *dest,unsigned int *destlen) {
+  register const unsigned char* s=(const unsigned char*) src;
+  unsigned long written=0,i;
+  for (i=0; s[i]; ++i) {
+    int j=fromhex(s[i]);
+    if (j<0) break;
+    dest[written]=j<<4;
+    j=fromhex(s[i+1]);
+    if (j<0) break;
+    dest[written]|=j;
+    i+=2;
+    ++written;
+  }
+  *destlen=written;
+  return i;
+}
