ffc91c62a32c16a1c8ef274377646110514e934a
leitner <leitner>
2002-08-05 18:10:01 +0000

    Imanuel Scholz corrected some comments

--- stralloc/stralloc_append.c
+++ stralloc/stralloc_append.c
@@ -1,7 +1,7 @@
 #include "stralloc.h"
 
-/* stralloc_append adds one byte buf[0] to the end of the string stored
- * in sa. It is the same as stralloc_catb(&sa,buf,1). */
+/* stralloc_append adds one byte in[0] to the end of the string stored                                            
+ * in sa. It is the same as stralloc_catb(&sa,in,1). */                                                           
 int stralloc_append(stralloc *sa,const char *in) {
   if (stralloc_readyplus(sa,1)) {
     sa->s[sa->len]=*in;
