33498d7c2d8761d82b7f1e318d2d0f81b89c35c8
leitner <leitner>
2003-05-04 16:20:08 +0000

    add small scan test

---
+++ test/scan_long.c
@@ -0,0 +1,22 @@
+#include <scan.h>
+#include <fmt.h>
+#include <buffer.h>
+#include <assert.h>
+
+main() {
+  char buf[1024];
+  unsigned long long int i;
+  if (sizeof(unsigned long) != 4)
+    return 0;
+  for (i=1; i<0xfffffffffull; i+=i+1) {
+    int k;
+    unsigned long test;
+    buf[k=fmt_ulonglong(buf,i)]=0;
+    buffer_puts(buffer_1,buf); buffer_putnlflush(buffer_1);
+    if (buf[scan_ulong(buf,&test)])
+      /* scan did not like the whole number */
+      assert(i>0xffffffffull);
+    else
+      assert(i<=0xffffffffull);
+  }
+}
