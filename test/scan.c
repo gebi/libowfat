4a7692f4d7fe4e992afbf63ff520029faa6c6485
leitner <leitner>
2003-05-27 21:44:29 +0000

    one more scan test

--- test/scan.c
+++ test/scan.c
@@ -12,5 +12,6 @@ int main() {
     assert(scan_ulong("4294967296",&l)==9 && l==429496729);
     assert(scan_ulong("42949672950",&l)==10 && l==4294967295ul);
     assert(scan_ulong("5294967295",&l)==9 && l==529496729);
+    assert(scan_ulong("4295067295",&l)==9 && l==429506729);
   }
 }
