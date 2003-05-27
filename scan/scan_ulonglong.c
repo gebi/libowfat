7f3c1ccc0c77365b873ab21aa2d7416ecb869eb2
leitner <leitner>
2003-05-27 20:31:25 +0000

    improve range check

--- scan/scan_ulonglong.c
+++ scan/scan_ulonglong.c
@@ -5,9 +5,13 @@ unsigned int scan_ulonglong(const char *src,unsigned long long *dest) {
   register unsigned long long l=0;
   register unsigned char c;
   while ((c=*tmp-'0')<10) {
-    unsigned long long m=l;
-    l=l*10+c;
-    if ((l>>3) < m) break;
+    unsigned long long n;
+    /* division is very slow on most architectures */
+    n=l<<3; if ((n>>3)!=l) break;
+    if (n+(l<<1) < n) break;
+    n+=l<<1;
+    if (n+c < n) break;
+    l=n+c;
     ++tmp;
   }
   *dest=l;
