1bd720d8a49cac9d164b8430cf0af6e7495b441f
leitner <leitner>
2003-09-08 17:37:23 +0000

    add io_timeouted

---
+++ io/io_timeouted.c
@@ -0,0 +1,17 @@
+#include "io_internal.h"
+
+int64 io_timeouted() {
+  tai6464 now;
+  static long ptr;
+  io_entry* e;
+  long alen=array_length(&io_fds,sizeof(io_entry));
+  taia_now(&now);
+  ++ptr;
+  if (ptr>=alen) ptr=0;
+  e=array_get(&io_fds,sizeof(io_entry),ptr);
+  if (!e) return -1;
+  for (;ptr<alen; ++ptr,++e)
+    if (e->inuse && taia_less(&e->timeout,&now))
+      return ptr;
+  return -1;
+}
