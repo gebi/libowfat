056760c5f191ac5cd073d0c262c753b980141783
leitner <leitner>
2003-09-19 19:08:51 +0000

    add cescape stuff and two tests

---
+++ test/textcode.c
@@ -0,0 +1,21 @@
+#include "array.h"
+#include "textcode.h"
+#include <assert.h>
+
+array a;
+
+int main() {
+  char buf[256];
+  int i;
+  for (i=0; i<256; ++i) buf[i]=i;
+
+  fmt_to_array(fmt_uuencoded,&a,buf,256);
+  assert(!array_failed(&a));
+  write(1,array_start(&a),array_bytes(&a));
+  array_trunc(&a);
+
+  fmt_to_array(fmt_base64,&a,buf,256);
+  assert(!array_failed(&a));
+  write(1,array_start(&a),array_bytes(&a)); write(1,"\n",1);
+  array_trunc(&a);
+}
---
+++ textcode/fmt_cescape.c
@@ -0,0 +1,46 @@
+#include "fmt.h"
+#include "textcode.h"
+#include "str.h"
+#include "haveinline.h"
+
+unsigned long fmt_cescape(char* dest,const char* src,unsigned long len) {
+  register const unsigned char* s=(const unsigned char*) src;
+  unsigned long written=0,i;
+  char c;
+  for (i=0; i<len; ++i) {
+    switch (s[i]) {
+    case '\a': c='a'; goto doescape;
+    case '\b': c='b'; goto doescape;
+    case '\e': c='e'; goto doescape;
+    case '\f': c='f'; goto doescape;
+    case '\n': c='n'; goto doescape;
+    case '\r': c='r'; goto doescape;
+    case '\t': c='t'; goto doescape;
+    case '\v': c='v'; goto doescape;
+    case '\\':
+	c='\\';
+      doescape:
+	if (dest) {
+	  dest[written]='\\';
+	  dest[written+1]=c;
+	}
+	written+=2;
+	break;
+    default:
+	if (s[i]<' ') {
+	  if (dest) {
+	    dest[written]='\\';
+	    dest[written+1]='x';
+	    dest[written+2]=fmt_tohex(s[i]>>4);
+	    dest[written+3]=fmt_tohex(s[i]&0xf);
+	  }
+	  written+=4;
+	} else {
+	  if (dest) dest[written]=s[i];
+	  ++written;
+	}
+	break;
+    }
+  }
+  return written;
+}
