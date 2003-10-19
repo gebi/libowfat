b01e72af692ba07a9cb252589abfe110df9c69b9
leitner <leitner>
2003-10-19 10:38:39 +0000

    quick fix to non-block issue

--- io/io_nonblock.c
+++ io/io_nonblock.c
@@ -5,7 +5,6 @@
 
 void io_nonblock(int64 d) {
   io_entry* e=array_get(&io_fds,sizeof(io_entry),d);
-  if (e && e->nonblock) return;
   fcntl(d,F_SETFL,fcntl(d,F_GETFL,0) | O_NDELAY);
   if (e) e->nonblock=1;
 }
