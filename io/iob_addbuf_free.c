0ba7b25005f0de35081ca824db28267d2678208c
leitner <leitner>
2003-10-31 23:42:04 +0000

    make iob_reset actually free the buffers

--- io/iob_addbuf_free.c
+++ io/iob_addbuf_free.c
@@ -1,5 +1,5 @@
 #include "iob_internal.h"
 
-int iob_addbuf(io_batch* b,const void* buf,uint64 n) {
+int iob_addbuf_free(io_batch* b,const void* buf,uint64 n) {
   return iob_addbuf_internal(b,buf,n,1);
 }
--- io/iob_reset.c
+++ io/iob_reset.c
@@ -3,6 +3,13 @@
 #include "iob_internal.h"
 
 void iob_reset(io_batch* b) {
+  unsigned long i,l;
+  iob_entry* x=array_start(&b->b);
+  l=array_length(&b->b,sizeof(iob_entry));
+  for (i=0; i<l; ++i) {
+    if (x[i].type==FROMBUF_FREE)
+      free((char*)x[i].buf);
+  }
   array_reset(&b->b);
   byte_zero(b,sizeof(*b));
 }
