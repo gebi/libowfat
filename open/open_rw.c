e0f6355a915be54369323f0576bcee3bf63869ac
leitner <leitner>
2004-02-04 23:02:35 +0000

    add a few man pages

---
+++ open/open_rw.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include "open.h"
+
+#ifndef O_NDELAY
+#define O_NDELAY 0
+#endif
+
+int open_rw(const char *filename) {
+  return open(filename,O_RDWR|O_CREAT|O_NDELAY,0644);
+}
---
+++ scan/scan_xlonglong.c
@@ -0,0 +1,13 @@
+#include "scan.h"
+
+unsigned int scan_xlonglong(const char* src,unsigned long long* dest) {
+  register const char *tmp=src;
+  register int l=0;
+  register unsigned char c;
+  while ((c=scan_fromhex(*tmp))<16) {
+    l=(l<<4)+c;
+    ++tmp;
+  }
+  *dest=l;
+  return tmp-src;
+}
