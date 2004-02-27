b27aa1a9d8863c01af7aa1a8535fc9d663248d5b
leitner <leitner>
2004-02-27 14:50:28 +0000

    better error handling

--- io/io_pipe.c
+++ io/io_pipe.c
@@ -11,14 +11,12 @@ int io_pipe(int64* d) {
   if (pipe(fds)==-1)
     return 0;
 #endif
-  if (io_fd(fds[1])) {
-    if (io_fd(fds[0])) {
-      d[0]=fds[0];
-      d[1]=fds[1];
-      return 1;
-    }
-    io_close(fds[1]);
+  if (io_fd(fds[1]) && io_fd(fds[0])) {
+    d[0]=fds[0];
+    d[1]=fds[1];
+    return 1;
   }
-  close(fds[0]);
+  io_close(fds[1]);
+  io_close(fds[0]);
   return 0;
 }
--- io/io_socketpair.c
+++ io/io_socketpair.c
@@ -11,14 +11,12 @@ int io_socketpair(int64* d) {
     if (socketpair(AF_INET6,SOCK_STREAM,IPPROTO_TCP,fds)==-1)
       if (socketpair(AF_INET,SOCK_STREAM,IPPROTO_TCP,fds)==-1)
 	return 0;
-  if (io_fd(fds[1])) {
-    if (io_fd(fds[0])) {
-      d[0]=fds[0];
-      d[1]=fds[1];
-      return 1;
-    }
-    io_close(fds[1]);
+  if (io_fd(fds[1]) && io_fd(fds[0])) {
+    d[0]=fds[0];
+    d[1]=fds[1];
+    return 1;
   }
-  close(fds[0]);
+  io_close(fds[1]);
+  io_close(fds[0]);
   return 0;
 }
