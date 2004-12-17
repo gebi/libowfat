e520740f094a23d188cf91a8a542c30a0ee44aa3
leitner <leitner>
2004-12-17 11:50:40 +0000

    add proxy, use errmsg.h in test/dnsip

--- test/dnsip.c
+++ test/dnsip.c
@@ -3,6 +3,7 @@
 #include <buffer.h>
 #include <errno.h>
 #include <string.h>
+#include <errmsg.h>
 
 int main(int argc,char* argv[]) {
   static char seed[128];
@@ -11,6 +12,7 @@ int main(int argc,char* argv[]) {
   char str[IP4_FMT];
   unsigned int i;
 
+  errmsg_iam("dnsip");
   (void)argc;
 
   dns_random_init(seed);
@@ -20,18 +22,12 @@ int main(int argc,char* argv[]) {
       buffer_putsflush(buffer_2,"out of memory\n");
       return 111;
     }
-    if (dns_ip4(&out,&fqdn) == -1) {
-      buffer_puts(buffer_2,"unable to find IP address for ");
-      buffer_puts(buffer_2,*argv);
-      buffer_puts(buffer_2,": ");
-      buffer_puterror(buffer_2);
-      buffer_putnlflush(buffer_2);
-      return 111;
-    }
+    if (dns_ip4(&out,&fqdn) == -1)
+      diesys(111,"unable to find IP address for ",*argv);
 
     for (i = 0;i + 4 <= out.len;i += 4) {
+      if (i) buffer_puts(buffer_1," ");
       buffer_put(buffer_1,str,ip4_fmt(str,out.s + i));
-      buffer_puts(buffer_1," ");
     }
     buffer_puts(buffer_1,"\n");
     ++argv;
