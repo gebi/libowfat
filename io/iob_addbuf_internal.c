11da04b65127ef6823042b5e4c2c7ae1f9844208
leitner <leitner>
2005-03-11 18:34:34 +0000

    fix descriptor leak in iob_addfile_close if the range was 0 (oops)

--- io/iob_addbuf_internal.c
+++ io/iob_addbuf_internal.c
@@ -1,8 +1,12 @@
 #include "iob_internal.h"
 
 int iob_addbuf_internal(io_batch* b,const void* buf,uint64 n,int _free) {
-  iob_entry* e=array_allocate(&b->b,sizeof(iob_entry),
-			     array_length(&b->b,sizeof(iob_entry)));
+  iob_entry* e;
+  if (!n) {
+    if (_free) free(buf);
+    return 1;
+  }
+  e=array_allocate(&b->b,sizeof(iob_entry),array_length(&b->b,sizeof(iob_entry)));
   if (!e) return 0;
   e->type=_free?FROMBUF_FREE:FROMBUF;
   e->fd=-1;
--- io/iob_addfile_close.c
+++ io/iob_addfile_close.c
@@ -2,7 +2,10 @@
 
 int iob_addfile_close(io_batch* b,int64 fd,uint64 off,uint64 n) {
   iob_entry* e;
-  if (n==0) return 1;
+  if (n==0) {
+    io_close(fd);
+    return 1;
+  }
   io_fd(fd);
   e=array_allocate(&b->b,sizeof(iob_entry),
 		   array_length(&b->b,sizeof(iob_entry)));
