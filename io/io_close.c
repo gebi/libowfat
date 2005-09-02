f65398ff2e67a298116917d3a197a8b7a3b699bb
leitner <leitner>
2005-09-02 21:52:22 +0000

    io_close resets the cookie

--- io/io_close.c
+++ io/io_close.c
@@ -11,6 +11,7 @@ void io_close(int64 d) {
   io_entry* e;
   if ((e=array_get(&io_fds,sizeof(io_entry),d))) {
     e->inuse=0;
+    e->cookie=0;
     io_dontwantread(d);
     io_dontwantwrite(d);
     if (e->mmapped) {
