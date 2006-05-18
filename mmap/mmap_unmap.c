818e9b9fe20270d3edc2981eec5efe36cfddd70b
leitner <leitner>
2006-05-18 06:13:49 +0000

    add mmap_unmap

---
+++ mmap/mmap_unmap.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <unistd.h>
+#ifdef __MINGW32__
+#include <windows.h>
+#else
+#include <sys/mman.h>
+#endif
+#include "open.h"
+#include "mmap.h"
+
+int mmap_unmap(char* mapped,unsigned long maplen) {
+#ifdef __MINGW32__
+  (void)maplen;
+  return UnmapViewOfFile(mapped)?0:-1;
+#else
+  return munmap(mapped,maplen);
+#endif
+}
