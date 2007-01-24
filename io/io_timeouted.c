e7ad98a89a441d9d5ae52d036045664d30e5c2bf
leitner <leitner>
2007-01-24 18:28:39 +0000

    fix really pathological case where io_timeouted would never     start over from the beginning because always new accept()ed     connections came in and got newer, higher descriptors since the last     io_timeouted loop.  (Dirk Engling)

--- io/io_timeouted.c
+++ io/io_timeouted.c
@@ -14,5 +14,8 @@ int64 io_timeouted() {
     if (e->inuse && e->timeout.sec.x && taia_less(&e->timeout,&now))
       return ptr;
   }
+  ptr=0;	/* this is for really pathological cases, where more
+		   connections come in all the time and so the timeout
+		   handling does not trigger initially */
   return -1;
 }
