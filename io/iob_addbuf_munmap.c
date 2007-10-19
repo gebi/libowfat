f7d8f4b225067f1711912514c81b46674accd777
leitner <leitner>
2007-10-19 10:59:47 +0000

    munmap cleanup fix (Erdgeist)

--- io/iob_addbuf_munmap.c
+++ io/iob_addbuf_munmap.c
@@ -3,7 +3,7 @@
 #include "iob_internal.h"
 
 static void cleanup(struct iob_entry* x) {
-  munmap((char*)x->buf,x->n);
+  munmap((char*)x->buf,x->offset+x->n);
 }
 
 int iob_addbuf_munmap(io_batch* b,const void* buf,uint64 n) {
