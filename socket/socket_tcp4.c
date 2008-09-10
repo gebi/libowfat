987e266efde474366940e71acd61d81ac365b08c
leitner <leitner>
2008-09-10 09:51:26 +0000

    check for socket() returning -1 before trying to set the socket non-blocking so we don't hide the "too many open files" error code

--- socket/socket_tcp4.c
+++ socket/socket_tcp4.c
@@ -4,6 +4,7 @@
 
 int socket_tcp4(void) {
   int s=socket_tcp4b();
+  if (s==-1) return -1;
   if (ndelay_on(s) == -1) { close(s); return -1; }
   return s;
 }
--- socket/socket_tcp6.c
+++ socket/socket_tcp6.c
@@ -4,6 +4,7 @@
 
 int socket_tcp6(void) {
   int s=socket_tcp6b();
+  if (s==-1) return -1;
   if (ndelay_on(s) == -1) { close(s); return -1; }
   return s;
 }
