0a42007dd930ccd216ad38ded8decd8a943f754f
leitner <leitner>
2014-02-25 18:36:36 +0000

    if called with srclen > maxlen, return maxlen not srclen

--- fmt/fmt_fill.c
+++ fmt/fmt_fill.c
@@ -8,12 +8,14 @@ size_t fmt_fill(char* dest,size_t srclen,size_t padlen,size_t maxlen) {
   long todo;
   char* olddest=dest;
   char* max=dest+maxlen;
+  if ((long)padlen<0 || (long)maxlen<0) return 0;
+  if (srclen>maxlen) return maxlen;
   if (dest==0) {
     unsigned long sum=srclen>padlen?srclen:padlen;
     return sum>maxlen?maxlen:sum;
   }
   dest+=srclen;
-  for (todo=padlen-srclen; todo>0; --todo) {
+  for (todo=(long)(padlen-srclen); todo>0; --todo) {
     if (dest>max) break;
     *dest=' '; ++dest;
   }
