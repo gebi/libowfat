ccce017aea3d9802a53f7489b5c2c402029aa5f8
leitner <leitner>
2015-03-17 00:01:00 +0000

    if SOCK_NONBLOCK is defined, use it instead of socket+fcntl

--- socket/socket_tcp4.c
+++ socket/socket_tcp4.c
@@ -1,10 +1,19 @@
+#include <sys/types.h>
+#ifndef __MINGW32__
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#endif
 #include "socket.h"
 #include "ndelay.h"
 
 int socket_tcp4(void) {
+#ifdef SOCK_NONBLOCK
+  return socket(PF_INET,SOCK_STREAM|SOCK_NONBLOCK,IPPROTO_TCP);
+#else
   int s=socket_tcp4b();
   if (s==-1) return -1;
   if (ndelay_on(s) == -1) { close(s); return -1; }
   return s;
+#endif
 }
--- socket/socket_tcp6.c
+++ socket/socket_tcp6.c
@@ -1,10 +1,21 @@
+#include <sys/types.h>
+#ifndef __MINGW32__
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#endif
+#include <errno.h>
+#include "haveip6.h"
 #include "socket.h"
 #include "ndelay.h"
 
 int socket_tcp6(void) {
+#if defined(LIBC_HAS_IP6) && defined(SOCK_NONBLOCK)
+  return socket(PF_INET6,SOCK_STREAM|SOCK_NONBLOCK,IPPROTO_TCP);
+#else
   int s=socket_tcp6b();
   if (s==-1) return -1;
   if (ndelay_on(s) == -1) { close(s); return -1; }
   return s;
+#endif
 }
