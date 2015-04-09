ee65422910cad9fd0f40af4a9c9835de289059cd
leitner <leitner>
2015-04-09 23:35:40 +0000

    make critbit0_contains as pure

--- critbit.h
+++ critbit.h
@@ -5,11 +5,18 @@
 extern "C" {
 #endif
 
+/* for __pure__ if we are compiling under dietlibc */
+#include <stddef.h>
+
+#ifndef __pure__
+#define __pure__
+#endif
+
 typedef struct {
   void *root;
 } critbit0_tree;
 
-int critbit0_contains(critbit0_tree *t, const char *u);
+int critbit0_contains(critbit0_tree *t, const char *u) __pure__;
 int critbit0_insert(critbit0_tree *t, const char *u);
 int critbit0_delete(critbit0_tree *t, const char *u);
 void critbit0_clear(critbit0_tree *t);
