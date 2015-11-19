2bbc7d10b004ca78fd1e555fad54f0b29461f94a
leitner <leitner>
2015-11-19 13:06:15 +0000

    slight optimization for buffer_flush

--- buffer/buffer_stubborn.c
+++ buffer/buffer_stubborn.c
@@ -3,11 +3,12 @@
 
 int buffer_stubborn(ssize_t (*op)(),int fd,const char* buf, size_t len,void* cookie) {
   ssize_t w;
+  errno=0;
   while (len) {
-    if ((w=op(fd,buf,len,cookie))<0) {
+    if ((w=op(fd,buf,len,cookie))<=0) {
       if (errno == EINTR) continue;
       return -1;
-    };
+    }
     buf+=w;
     len-=(size_t)w;
   }
