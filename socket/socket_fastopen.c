7114febcf72fff3e1e3f83979655f776e519a4c1
leitner <leitner>
2016-04-07 12:08:05 +0000

    attempt to support FreeBSD

--- socket/socket_fastopen.c
+++ socket/socket_fastopen.c
@@ -2,9 +2,14 @@
 #ifndef __MINGW32__
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <netinet/tcp.h>
 #endif
 #include <errno.h>
+
+#if defined(TCP_FASTOPEN) && !defined(SOL_TCP) && defined(IPPROTO_TCP)
+#define SOL_TCP IPPROTO_TCP
+#endif
 
 int socket_fastopen(int s) {
 #if defined(SOL_TCP) && defined(TCP_FASTOPEN)
