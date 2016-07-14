a54a88d7e6b4c9733ddfd143e8ccfcfc0cbe4f1e
leitner <leitner>
2016-07-14 16:19:47 +0000

    add mmap_readat

--- mmap.h
+++ mmap.h
@@ -12,6 +12,9 @@ extern "C" {
  * map in filesize and return pointer to map. */
 const char* mmap_read(const char *filename,size_t* filesize);
 
+/* like mmap_read but use openat instead of open */
+const char* mmap_readat(const char *filename,size_t* filesize,int dirfd);
+
 /* open file for writing, mmap whole file privately (copy on write),
  * close file, write length of map in filesize and return pointer to
  * map. */
