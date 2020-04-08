e5fa7f4bc9b19982e3989a827ac2268b8a4db253
leitner <leitner>
2020-04-08 11:37:31 +0000

    use _Static_assert if available

--- compiletimeassert.h
+++ compiletimeassert.h
@@ -1,4 +1,12 @@
 
+#if __STDC_VERSION__ >= 201112L
+
+#define compiletimeassert(cond) _Static_assert(cond)
+
+#else
+
 #define __X(x, y) x ## y
 #define __Y(x, y) __X(x, y)
 #define compiletimeassert(cond) struct __Y(foo,__LINE__) { char __temp[1 - (!(cond))*2]; };
+
+#endif
